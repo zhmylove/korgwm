0b8cf685d3ee524caba411ee245578efe19d50ce
Sergei Zhmylev <zhmylove@narod.ru>
2023-08-10 22:42:39 +0300

    Add basic hotkeys support

--- korgwm.pl
+++ korgwm.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 use feature 'signatures';
-use lib 'lib';
+use lib 'lib', '../X11-Xlib-0.23-work/lib', '../X11-Xlib-0.23-work/blib/arch';
 use open ':std', ':encoding(UTF-8)';
 use utf8;
 use X11::Xlib ':all';
@@ -20,17 +20,18 @@ my $X = X11::Protocol->new();
 $X->init_extension("XINERAMA") or die "XINERAMA not available";
 
 my $display = X11::Xlib->new();
-
-printf STDERR "Screen count: %s\n", $display->screen_count(); # TODO remove
 die "Use XINERAMA or die!" unless $display->screen_count() == 1;
-
 my $screen = $display->screen(0);
 
 # There is only one root window
 my $root = $screen->root_window;
 
+# Become a WM
+my $error_caught = undef;
+X11::Xlib::on_error(sub { $error_caught = 1 });
 $root->event_mask_include(SubstructureRedirectMask, SubstructureNotifyMask);
-$display->flush();
+$display->flush_sync();
+die "Looks like other WM is running" if $error_caught;
 
 # Detect real monitors
 my $screens;
@@ -54,19 +55,69 @@ if ($X->XineramaIsActive()) {
 
 warn Dumper $screens; # TODO remove
 
-for(;;) {
-    # TODO rewrite event loop to handle events from Xlib, Gtk and AE and set timeout = 0
+# Functions for keyboard events
+## We're not interested in CHLD
+$SIG{CHLD} = "IGNORE";
 
-    # Process events from each screen panel
-    $screens->{$_}->{panel}->iter() for keys %{ $screens };
+## Run new process
+sub run_cmd($args) {
+    my $cmd = defined $args ? $args->{cmd} : undef;
+    next unless defined $cmd;
+    my $pid = fork;
+    die "Cannot fork(2)" unless defined $pid;
+    return if $pid;
+    exec $cmd;
+    die "Cannot execute $cmd";
+    ...;
+}
 
-    my $evt = $display->wait_event(timeout => 0.01);
-    next unless defined $evt; # TODO check if wait_event could be interrupted
-    my $ref = ref $evt;
+# TODO move to some config
+# Prepare configuration for hotkeys
+my @keys_config = (
+    {
+        key => 'p',
+        mod => Mod4Mask,
+        cb => \&run_cmd,
+        args => { cmd => 'xterm -font "xft:DejaVu Sans Mono" -fs 14 -g 150x40' }
+    },
+    {
+        key => 'q',
+        mod => Mod4Mask,
+        cb => sub { exit 0 },
+        args => {}
+    },
+);
 
-    printf STDERR "Evt: %s (w:%s)\n", $ref, $evt->window // "undef";
+# Initialize WM keybindings
+my $keys_cb;
+for my $hotkey (@keys_config) {
+    my $keycode = $display->keymap->find_keycode($hotkey->{key});
+    XGrabKey($display, $keycode, $hotkey->{mod}, $root, 0, GrabModeAsync, GrabModeAsync);
+    $keys_cb->{$keycode} = sub { $hotkey->{cb}->($hotkey->{args}) };
+}
+$display->flush_sync();
 
-    if ($ref eq "X11::Xlib::XMapRequestEvent") {
+# Handlers for events
+my $evt_cb = {
+    "X11::Xlib::XConfigureRequestEvent" => sub ($evt) {
+        my %changes;
+
+        # TODO deal somehow with fullscreen
+        # TODO handle somehow modal windows
+        # TODO process existing windows to move them below the panel and set their size
+        $changes{$_} = $evt->$_ for qw( x y width height border_width );
+        $changes{y} = $panel_height if $panel_height > $changes{y} // $panel_height;
+        $changes{sibling} = $evt->above;
+        $changes{stack_mode} = $evt->detail;
+        XConfigureWindow($display, $evt->window, $evt->value_mask | CWY, \%changes);
+        $display->flush();
+    },
+    "X11::Xlib::XKeyEvent" => sub ($evt) {
+        return unless $evt->type == KeyPress;
+        return unless defined (my $cb = $keys_cb->{$evt->keycode});
+        $cb->();
+    },
+    "X11::Xlib::XMapRequestEvent" => sub ($evt) {
         # TODO resize window here ..?
 
         my $win = X11::Xlib::Window->new(display => $display, xid => $evt->window);
@@ -89,27 +140,26 @@ for(;;) {
 
         $win->show();
         $display->flush();
-        next;
+    },
+    "X11::Xlib::XConfigureEvent" => sub ($evt) {
+        warn Dumper [map { $evt->$_ } qw( above border_width event height override_redirect width window x y )];
+    },
+};
+
+for(;;) {
+    # TODO (obsoleted, but what is timeout?) rewrite event loop to handle events from Xlib, Gtk and AE and set timeout = 0
+
+    # Process Gtk events from each screen panel
+    $screens->{$_}->{panel}->iter() for keys %{ $screens };
+
+    # Process Xlib events
+    if (defined(my $evt = $display->wait_event(timeout => 0.01))) {
+        # TODO check if wait_event could be interrupted
+        my $ref = ref $evt;
+        my $cb = $evt_cb->{$ref};
+        printf STDERR "Evt: %s (w:%s) handler: %s\n", $ref, $evt->window // "undef", $cb // "IGNORE";
+        defined $cb && $cb->($evt);
     }
 
-    if ($ref eq "X11::Xlib::XConfigureRequestEvent") {
-        my %changes;
-
-        # TODO deal somehow with fullscreen
-        # TODO handle somehow modal windows
-        # TODO process existing windows to move them below the panel and set their size
-        $changes{$_} = $evt->$_ for qw( x y width height border_width );
-        $changes{y} = $panel_height if $panel_height > $changes{y} // $panel_height;
-        $changes{sibling} = $evt->above;
-        $changes{stack_mode} = $evt->detail;
-        XConfigureWindow($display, $evt->window, $evt->value_mask | CWY, \%changes);
-        $display->flush();
-        next;
-    }
-
-    if ($ref eq "X11::Xlib::XDestroyWindowEvent") {
-        #die "$0 is exiting now on any window close";
-    }
-
-    print STDERR "... ignored\n";
+    # TODO Process AE events here
 }
